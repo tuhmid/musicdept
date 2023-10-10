33670f33b195428ea5358754d4ce82325def1746
Rhys Frazier <rhysrfrazier@gmail.com>
2023-10-10 15:13:19 -0400

    Added headphones model and debugged

--- server.js
+++ server.js
@@ -15,4 +15,8 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 
+//pathways go here
+
+app.get('/', (req,res) => res.send('The root of the stereo department'))
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
